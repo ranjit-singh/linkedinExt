45e71dac4cca2148249a04039666cc15e39e41ed
ranjit-singh <sendranjit123@gmail.com>
2015-12-16 18:37:07 +0530

    search linked in profile

--- development/scripts/contentscript.js
+++ development/scripts/contentscript.js
@@ -17,12 +17,8 @@ window.addEventListener("load", function() {
   });
   var autoCompleteWin = document.querySelector('.gssb_e');
   angular.element(autoCompleteWin).remove();
-  var pageInfos=[];
   $('#search').find('a').each(function() {
-      var pageInfo = {};
-
       var href = $(this).attr('href');
-
       if (href != null && href.indexOf("http") == 0 && href.indexOf('linkedin')!=-1)
       {
           //only add urls that start with http
